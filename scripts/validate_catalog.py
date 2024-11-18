378e497f0b653fc2d7e2e16ad7e56a4cb4ccf0a7
LukasKuzavas <101813477+LukasKuzavas@users.noreply.github.com>
2024-11-18 13:51:30 +0000

    feat: add catalog validation workflow (CIDE-81) (#7)

---
+++ scripts/validate_catalog.py
@@ -0,0 +1,69 @@
+import json
+import os
+import requests
+from jsonschema import validate, ValidationError
+from collections import Counter
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CATALOG_PATH = os.path.join(SCRIPT_DIR, "../genesis-create-templates/catalog.json")
+SCHEMA_PATH = os.path.join(SCRIPT_DIR, "../genesis-create-templates/schema.json")
+
+
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def validate_schema(catalog, schema):
+    try:
+        validate(instance=catalog, schema=schema)
+    except ValidationError as e:
+        print(f"Schema validation error: {e.message}")
+        exit(1)
+
+def check_unique_names(catalog):
+    names = [item['name'] for item in catalog]
+    names_counter = Counter(names)
+    duplicates = [name for name, count in  names_counter.items() if count > 1]
+
+    if duplicates:
+        print(f"Error: Duplicate names found in the catalog: {', '.join(duplicates)}")
+        exit(1)
+
+def check_repositories(catalog):
+    failed_repos = []
+
+    for item in catalog:
+        repo_url = item.get('repo')
+        if not repo_url:
+            failed_repos.append(f"Missing 'repository' field in item {item}")
+            continue
+        try:
+            response = requests.get(f"https://github.com/{repo_url}")
+            if response.status_code != 200:
+                failed_repos.append(f"Repository '{repo_url}' not retrievable.")
+                continue
+        except requests.RequestException as e:
+            failed_repos.append(f"Failed to access repository '{repo_url}': {e}")
+            continue
+
+    if failed_repos:
+        print("Error: the following repository checks failed:")
+        for error in failed_repos:
+            print(f" - {error}")
+        exit(1)
+
+def main():
+    catalog = load_json(CATALOG_PATH)
+    schema = load_json(SCHEMA_PATH)
+
+    validate_schema(catalog, schema)
+
+    check_unique_names(catalog)
+
+    check_repositories(catalog)
+
+    print("Validation passed.")
+
+if __name__ == "__main__":
+    main()
